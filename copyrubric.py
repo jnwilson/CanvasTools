5d0c4525d11758d49057812e19879309b19306ce
Joseph N. Wilson <jnw@cise.ufl.edu>
2021-03-30 16:38:29 -0400

    Got rid of pep complaints

--- copyrubric.py
+++ copyrubric.py
@@ -20,21 +20,26 @@ def main():
     ##
     # read student file
     try:
-        student_file = open(args.students,'r')
-    except:
-        sys.stderr.write('Unable to open student file: [' + args.students + ']\n')
-        sys.stderr.write('Exception info: ' + str(sys.exc_info()[0]))
+        student_file = open(args.students, 'r')
+    except Exception as err:
+        print(f'Unable to open student file: [{args.students}]: {str(err)}',
+              file=sys.stderr,
+              flush=True)
         exit(1)
 
+    # noinspection PyUnboundLocalVariable
     student_list = student_file.readlines()
     for entry in student_list:
         new_filename = entry.rstrip() + '.xlsx'
         try:
             shutil.copy2(args.rubric, new_filename)
             print(new_filename.rstrip())
-        except:
-            sys.stderr.write('Unable to create xlsx file for [' + new_filename + ']\n')
-            sys.stderr.write('Exception info: ' + str(sys.exc_info()) + '\n')
+        except Exception as err:
+            print(f'Unable to create xlsx file for [{new_filename}]: {str(err)}',
+                  file=sys.stderr,
+                  flush=True)
+            exit(1)
+
 
 if __name__ == '__main__':
     main()
