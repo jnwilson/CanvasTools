325eb81764e4c4ca28a539d9061cdb998bb078dd
Joseph N. Wilson <jnw@cise.ufl.edu>
2021-09-23 16:07:01 -0400

    Initial version (supports pdf or docx assignment files)

---
+++ grade_assignment.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh -x
+#
+# grade_assignment.sh
+#
+# Assumes directory contains downloaded canvas assignment pdf file with filenames of form
+#    lastname_firstnameXXXXXX_question_######_#######_assignmentname.pdf
+# and .xlsx grading spreadsheets with filenames of form
+#    lastname_firstnameXXXXXX:YYYYYY.xlsx
+# where XXXXXX is the Canvas Student ID and
+# YYYYYY is the Canvas Course Shell ID
+#
+# successively an xlsx spreadsheet and the pdf file for a student.
+# When grading for a student is complete, close these files and press return in the shell
+# where grade_assignment.sh was run.
+#
+# After grading an asignment use submit_assignment.py to submit grades to canvas
+#
+
+for x in *.xlsx; do
+    base=${x/:*/}
+    pdf=$base*.pdf
+    if [ -e $base*.pdf ]; then open $base*.pdf; \
+    else if [ -e $base*.docx ]; then open $base*.docx; fi; fi
+    open "$x"
+    read
+    done
+    
