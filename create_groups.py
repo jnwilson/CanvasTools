055e4fda5cd457442d83744eac1a5edd774177f3
Joseph N. Wilson <jnwwilson@gmail.com>
2022-09-26 16:04:01 -0400

    Initial version

--- create_groups.py
+++ create_groups.py
@@ -53,6 +53,8 @@ def main():
 
     args = parser.parse_args()
     uri_base = f'https://ufl.instructure.com/api/v1/'
+    dry_run = args.n
+    indent_string = '\n   '
     ##
     # read Canvas token from file
     ##
@@ -81,17 +83,18 @@ def main():
             # read gradefile column_names
             column_names = next(csv_reader)
             column_list = list(filter(lambda x: not x.find('Attendance'), column_names))
-            column_number = column_names.index(column_list[0])
+            attendance_column_number = column_names.index(column_list[0])
             assert len(column_list) == 1, 'Too many column names contain the word "Attendance"'
-            # skip extra lines
+
+            # Skip extra header lines in grades file
             next(csv_reader)
             next(csv_reader)
 
             for row in csv_reader:
-                if row[column_number] and int(float(row[column_number])) == 1:
-                    in_class_students.append(row[SIS_ID_Index])
+                if row[attendance_column_number] and int(float(row[attendance_column_number])) == 1:
+                    in_class_students.append(row)
                 else:
-                    online_students.append(row[SIS_ID_Index])
+                    online_students.append(row)
 
     except Exception as err:
         print(f"Couldn't read csv file {args.csv_file}\n   {err}",
@@ -115,12 +118,16 @@ def main():
             group_categories_uri = f'{uri_base}courses/{args.cid}/group_categories'
             if args.debug:
                 print(f"uri = {group_categories_uri}")
-            group_categories_response = requests.post(url=group_categories_uri,
-                                                      headers=headers,
-                                                      params=group_categories_rq_params)
-            group_category_object = json.loads(group_categories_response.text)
-            if args.debug:
-                print(f'group_categories_response: {group_categories_response.text}')
+            if not dry_run:
+                group_categories_response = requests.post(url=group_categories_uri,
+                                                          headers=headers,
+                                                          params=group_categories_rq_params)
+                group_category_object = json.loads(group_categories_response.text)
+                if args.debug:
+                    print(f'group_categories_response: {group_categories_response.text}')
+            else:
+                group_category_object = {'id': 0}
+                print(f'group_category_object [{group_category_object}]')
         except Exception as err:
             print(err)
             print(f'* Create group categories request failed: {group_categories_response.text}\n   {err}',
@@ -140,20 +147,31 @@ def main():
         # do groups of 3
         raw_data += str.encode('user_id,group_name,status\r\n')
         os.write(temp_fd, str.encode('user_id,group_name,status\r\n'))
+        if args.debug:
+            print(f'len(in_class_permutation): {len(in_class_permutation)}')
+            print(f'len(online_permutation): {len(online_permutation)}')
         for this_permutation in (in_class_permutation, online_permutation):
+            #if args.debug:
+            #    all = this_permutation[0:]
+            #    print(f'this_permutation: {indent_string.join(all)}')
             while len(this_permutation) > 4 or len(this_permutation) == 3:
+                if args.debug:
+                    print(f"In 3's length is :{len(this_permutation)}")
                 group_members = this_permutation[:3]
                 this_permutation = this_permutation[3:]
                 for group_member in group_members:
-                    raw_data += str.encode(f'{group_member},{assignment} {str(group_number)},accepted\r\n')
-                    os.write(temp_fd, str.encode(f'{group_member},{assignment} {str(group_number)},accepted\r\n'))
+                    raw_data += str.encode(f'{str(group_member[SIS_ID_Index]).zfill(8)},{assignment} {str(group_number)},accepted\r\n')
+                    os.write(temp_fd, str.encode(f'{group_member[0]},{str(group_member[SIS_ID_Index]).zfill(8)},{assignment} {str(group_number)},accepted\r\n'))
                 group_number += 1
-            while len(this_permutation) >2:
+
+            while len(this_permutation) >=2:
+                if args.debug:
+                    print(f"In 2's length is: {len(this_permutation)}")
                 group_members = this_permutation[:2]
                 this_permutation = this_permutation[2:]
                 for group_member in group_members:
-                    raw_data += str.encode(f'{group_member},{assignment} {str(group_number)},accepted\r\n')
-                    os.write(temp_fd, str.encode(f'{group_member},{assignment} {str(group_number)},accepted\r\n'))
+                    raw_data += str.encode(f'{str(group_member[SIS_ID_Index]).zfill(8)},{assignment} {str(group_number)},accepted\r\n')
+                    os.write(temp_fd, str.encode(f'{group_member[0]},{str(group_member[SIS_ID_Index]).zfill(8)},{assignment} {str(group_number)},accepted\r\n'))
                 group_number += 1
 
         try:
@@ -162,6 +180,9 @@ def main():
             files = {'attachment': open(temp_path,'rb')}
             if args.debug:
                 print(f'import_uri {import_uri}')
+            if dry_run:
+                # print(f'requested groups:\n{raw_data}')
+                continue
             import_response = requests.post(url=import_uri,
                                             data=raw_data,
                                             headers=csv_headers)
